0502527a4ad891d9bb5f4e29923136b6d9791342
Braden Stanfield <bls6151@utulsa.edu>
2024-03-08 15:01:58 -0600

    still working on 3

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -5,9 +5,15 @@ def courseGrade():
       
       
     # TODO: Read a file name from the user and read the tsv file here. 
-   
-   
+    bot = input()
+    stuff = open(bot).read().split()
+    h = len(stuff)/5
+    i = 0
+    total = 0
+    num = 0
     # TODO: Compute student grades and exam averages, then output results to a text file here. 
+    while i < h:
+        total += 
     return
 
 if __name__ == "__main__":

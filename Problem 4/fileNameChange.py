d9dd1c62e847854218d30a82b24dda0fa7080feb
Braden Stanfield <bls6151@utulsa.edu>
2024-03-15 15:14:49 -0500

    problem 4

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,7 +1,19 @@
 #Name: Braden Stanfield  Lab: Fri 3
 def fileNameChange():
     #type your code here
-
+    text = input()
+    stuff = open(text).read().split()
+    i = 0
+    und = 0
+    spilt = ""
+    new = ""
+    while i < len(stuff):
+        und = stuff[i].find("_")
+        spilt = stuff[i].split("_")
+        new += spilt[0] + "_info.txt\n"
+        i += 1
+    
+    print(new)
     return
 
 if __name__ == '__main__':
